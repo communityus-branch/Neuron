c048e40a6c887021e3af92fe9309a3a94c8fc1a9
Trojaner25 <trojaner25@gmail.com>
2016-02-02 22:19:49 +0200

    Internal code name for this game: Neuron

--- Assets/Static_Interface/The_Collapse/GameInfo.cs
+++ Assets/Static_Interface/The_Collapse/GameInfo.cs
@@ -6,7 +6,7 @@ namespace Static_Interface.The_Collapse
 {
     public class GameInfo
     {
-        public const string NAME = "Sample text";
+        public const string NAME = "Neuron";
         public const string VERSION = "1.0.0.0";
         public static AppId_t ID = new AppId_t(480);
 
